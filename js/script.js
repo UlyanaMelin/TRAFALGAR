90fc34b623fb1709f970135cbd0e4381562ef6ad
Ulyana Melnikova <umelnikova090@gmail.com>
2023-07-13 15:45:18 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    items: 1,
+    navText: [
+        '<svg width="29" height="19" fill="none"><path d="M27 8H6l4-5V1H8L1 8v2l7 8h2v-2l-4-5h21l1-2-1-1z" fill="#458FF6"/></svg>',
+        '<svg width="29" height="19" fill="none"><path d="M2 11h21l-5 5v2h2l8-7V9l-8-8h-2v2l5 5H2l-2 2 2 1z" fill="#458FF6"/></svg>'
+]
+});
